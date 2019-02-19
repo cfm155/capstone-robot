1c74a8e4eeaf325d39d4357bf63fb0f4168d2858
Carter McCall <cartermccall@Carters-MacBook-Air.local>
2019-02-19 15:48:07 -0600

    first commit

---
+++ test.py
@@ -0,0 +1,8 @@
+from ev3dev.ev3 import *
+from time import sleep
+m = LargeMotor('outA')
+m.run_timed(time_sp=3000, speed_sp=-750)
+print("set speed (speed_sp) = " + str(m.speed_sp))
+sleep(1)  # it takes a moment for the motor to start moving
+print("actual speed = " + str(m.speed))
+sleep(4)
