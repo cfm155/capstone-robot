84d13859cbac2a582ec3d0f8fbbbd84c148aa2fc
Carter McCall <cartermccall@Carters-Air.ddns.drake.edu>
2019-02-28 17:18:46 -0600

    write to file, read from file

---
+++ connect.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ssh -T robot@ev3dev.local
+echo maker\n
+exit
---
+++ writetochar.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+ofile = open('char.txt','w')
+ofile.write('t')
+ofile.close()
+ofile = open('char.txt','w')
+ofile.write('C')
+ofile.close()
